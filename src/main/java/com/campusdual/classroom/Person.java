3afe042070eb0ce0d3e51a373032f8cad44e4bea
Ana Garces <anagarcestech@gmail.com>
2024-11-18 18:21:20 +0100

    Ejercicio 14 resuelto

--- src/main/java/com/campusdual/classroom/Person.java
+++ src/main/java/com/campusdual/classroom/Person.java
@@ -8,4 +8,13 @@ public class Person {
 		this.name = name;
 		this.surname = surname;
 	}
+
+	public void getDetails(){
+		StringBuilder builder = new StringBuilder();
+		builder.append("Mi nombre es: ");
+		builder.append(this.name);
+		builder.append(" Mi apellido es: ");
+		builder.append(this.surname);
+		System.out.println(builder.toString());
+	}
 }
